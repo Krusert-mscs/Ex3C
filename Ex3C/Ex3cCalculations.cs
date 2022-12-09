59ff2add47fd05cfe0386ee17b81a0620c20ff1f
Keegan <Keegan@DESKTOP-2RO8Q56>
2022-12-09 17:34:10 -0600

    Add project files.

---
+++ Ex3C/Ex3cCalculations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3C
+{
+    public class Ex3cCalculations
+    {
+        public static string Calc0(int index)
+        {
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            return "";
+        }
+
+        public static void Calc1()
+        {
+            string[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+            string[] hours = { "Closed", "10am - 6pm", "10am - 6pm", "10am - 6pm", "10am - 9pm", "10am - 6pm", "8am - 4pm" };
+
+            
+        }
+
+        public static void Calc2(int numbers)
+        {
+            int[] numbers = { 2, 16, 7, 22, 35 };
+            
+        }
+        public static void Calc3(double[] numbers)
+        {
+            double[] numbers = { 2.3, 16.1, 7.9, 22.3, 35.4 };
+        }
+        public static void Calc5(double[] numbers)
+        {
+            double[] numbers = { 2.3, 16.1, 7.9, 22.3, 35.4 };
+        }
+
+        public static double[] Calc6(double[] numbers)
+        {
+            return new double[10];
+        }
+    }
+}
---
+++ krusert3c1UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("krusert3c1UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("krusert3c1UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a872847a-e3bb-4a5c-8520-e5352780f854")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
